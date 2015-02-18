8e7f8340e7a350d1091d0fdf03580a7a0d71098f
Michal Seta <djiamnot@gmail.com>
2015-02-17 23:52:48 -0500

    generate random positions

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,13 @@
+from random import random
+
 def scale (OldValue, OldMin, OldMax, NewMin, NewMax):
     OldRange = (OldMax - OldMin)  
     NewRange = (NewMax - NewMin)  
     NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
     return NewValue
+
+def randomPosition():
+    x = random() * 8 -4
+    y = random() * 8 -4
+    z = random() * 4
+    return [x, y, z]
