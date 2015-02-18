559fedda70bc9b93446973230ce896c6c13742b2
Michal Seta <djiamnot@gmail.com>
2015-02-17 23:52:29 -0500

    add more controls to choir

--- scripts/config.py
+++ scripts/config.py
@@ -42,7 +42,7 @@ Tele = {
 
 Choir = {
     'instruments': ['C1V1','C1V2','C1V3','C1V4','C1V5','C1V6','C1V7','C1V8','C1V9','C1V10','C2V1','C2V2','C2V3','C2V4','C2V5','C2V6','C2V7','C2V8','C2V9','C2V10'],
-    'controls': ['valve/x', 'speed/x', 'dur/x']
+    'controls': ['valve/x', 'speed/x', 'dur/x', 'onoff/x', 'open/x']
 }
 
 groups =  {'ThePipe': ThePipe, 'Tele': Tele, 'Choir': Choir}
